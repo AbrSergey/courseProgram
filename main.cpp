b3f51e8b04c083323134ec78330b1a2a26169928
Sergey <sergey.abramov@gmail.com>
2017-05-19 19:54:38 +0700

    Edit manin.cpp

--- main.cpp
+++ main.cpp
@@ -8,23 +8,39 @@ using namespace std;
 
 int main()
 {
-    int lenRezult = 10;
-    int numberArg = 2;
-    int lenSum = 2;
-    int numberStates = 1 << numberArg;
+    int lenRezult = 10; // длина в битах случайного числа
+    int numberArg = 2;  // количество аргументов в функции f - полином Жегалкина
+    int lenSum = 2; // количество сумм в функции f - полином Жегалкина
+    int numberStates = 1 << numberArg;  // количество состояний
 
     unsigned int *setStates = new unsigned int [numberStates];
 
     for (int i = 0; i < numberStates - 1; i++)
-        setStates[i] = i + 1;
+        setStates[i] = i + 1;   // формируем функцию переходов g
 
-    setStates[numberStates - 1] = 0;
+    setStates[numberStates - 1] = 0;    // формируем функцию переходов g
 
     unsigned int * sum;
 
-    sum = inputRandom(numberArg, lenSum);
+    sum = inputRandom(numberArg, lenSum);   // генерация функции f - полинома Жегалкина
 
-    unsigned int x = generatorA(setStates, numberStates, sum, lenSum, lenRezult);
 
-    cout << "generatorA = " << x << endl;
+    // //////////////////////////////////////////////////
+    unsigned int result = 0;
+
+    // Generator A
+
+    for (int i = 0; i < lenRezult - 1; i++){
+
+        result |= polynomZhegalkina(setStates[i], sum, lenSum);
+
+        result <<= 1;
+    }
+
+    result |= polynomZhegalkina(setStates[lenRezult - 1], sum, lenSum);
+    // ////////////////////////////////////////////////
+
+
+
+    cout << "generatorA = " << result << endl;
 }
